e42b9082a9eed0578db2e44fc63dc31de57b563a
unknown <lsmxy4@gmail.com>
2026-02-10 12:21:04 +0900

    router

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,25 +2,32 @@ import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
+import './styles/Home.css'
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { Pagination,Navigation,Autoplay } from 'swiper/modules';
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
+import { homeSlides } from '../data/homeSlide'
 
 const Home = () => {
     return (
-        <section>
+        <section >
             <div className="inner">
                 <h2>home</h2>
                 <Swiper
                     navigation={true}
-                    autoplay = {true}
+                    autoplay={true}
                     loop
-                    pagination={{ clickable: true }}
-                    modules={[Navigation, Pagination,Autoplay]}
+                    pagination={true}
+                    modules={[Navigation, Pagination, Autoplay]}
                     className="mySwiper">
-                    <SwiperSlide>Slide 1</SwiperSlide>
-                    <SwiperSlide>Slide 2</SwiperSlide>
-                    <SwiperSlide>Slide 3</SwiperSlide>
+                    {homeSlides.map(({ id, title, img, subtitle }) => (
+                        <SwiperSlide key={id} style={{ backgroundImage: `url(${img})` }}>
+                            <div className="slide-text">
+                                <h4>{title}</h4>
+                                <p>{subtitle}</p>
+                            </div>
+                        </SwiperSlide>
+                    ))}
                 </Swiper>
             </div>
         </section>
